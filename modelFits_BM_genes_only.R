2c7ca69468291e6f18e5a8fe624fc0be43b06306
fieldima <60451947+fieldima@users.noreply.github.com>
2022-02-12 17:20:30 -0800

    First phylogenetic significance test, on just the BM genes.

--- modelFits_BM_genes_only.R
+++ modelFits_BM_genes_only.R
@@ -118,3 +118,42 @@ saveRDS(p_piv, file = "only_BM_allfit_pvalues_table")
 
 p_piv %>% ggplot(aes(value)) + geom_histogram(aes(y = ..density..)) + facet_wrap(~tstat, nrow = 1) + theme_bw()
 ggsave("only_BM_allfit_arbutus_results.png")
+
+#Now checking the phylogenetic signal of one set of genes and comparing to other
+num <- 1:dim(pvals)[1]
+only_NA <- pvals %>% mutate(num = num) %>% filter(is.na(s.hgt)) %>% pull(num)
+not_NA <- pvals %>% mutate(num = num) %>% filter(!is.na(s.hgt)) %>% pull(num)
+
+only_NA_df <- bm_only[,only_NA]
+not_NA_df <- bm_only[,not_NA]
+
+#Null hypothesis of randomization test is that there is no phylogenetic signal,
+#so low p-value indicates that there IS phylogenetic signal
+
+run_phylosig <- function (phy, df) {
+  sig_results <- vector("list", length = ncol(df))
+  for(i in 1:length(sig_results)){
+    res <- phylosig(phy, df[,i], method = "K", test = TRUE)
+    sig_results[[i]] <- res
+  }
+  sig_results
+}
+
+only_NA_sig <- run_phylosig(cleaned$phy, only_NA_df)
+not_NA_sig <- run_phylosig(cleaned$phy, not_NA_df)
+
+get_K_and_P <- function (sig) {
+  K <- sig$K
+  P <- sig$P
+  list("K" = K, "P-value" = P)
+}
+
+only_NA_vals <- map_df(only_NA_sig, get_K_and_P) %>% mutate(subset = "only_NA")
+not_NA_vals <- map_df(not_NA_sig, get_K_and_P) %>% mutate(subset = "not_NA")
+
+K_P_vals <- bind_rows(only_NA_vals, not_NA_vals) %>% pivot_longer(cols = c(K, `P-value`), names_to = "Statistic")
+
+K_P_vals %>% ggplot(aes(x = subset, y = value, fill = subset)) + geom_violin() + 
+  geom_boxplot() + facet_wrap(~Statistic) + theme_bw() + ggtitle("Phylogenetic signal of BM-only genes")+
+  xlab("Subset of data")
+ggsave("phylosig.png")
